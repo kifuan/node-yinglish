f43d3ac14bd963da2f473745b9f47455d9ca4639
kifuan <kifuan@foxmail.com>
2022-11-07 17:45:04 +0800

    refactor: use functions rather than object getters

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,20 @@ function probably(probability: number): boolean {
   return Math.random() < probability
 }
 
-const fragments = {
-  ellipsis(l: number) {
-    return probably(l) ? '...' : this.emoji
-  },
-  get emoji() {
-    const emojis = ['😍', '❤', '🥺', '🥵', '🥰']
-    return emojis[Math.floor(Math.random() * emojis.length)]
-  },
+function generateEllipsis(level: number) {
+  return probably(level) ? '...' : generateEmoji()
+}
+
+function generateEmoji() {
+  const emojis = ['😍', '❤', '🥺', '🥵', '🥰']
+  return emojis[Math.floor(Math.random() * emojis.length)]
 }
 
 function convertWord({ word, tag }: TaggedWord, level: number): string {
   if (!probably(level))
     return word
 
-  word = word.replace(/,|，|。/g, fragments.ellipsis(level)).replace(/!|！/g, fragments.emoji)
+  word = word.replace(/,|，|。/g, generateEllipsis(level)).replace(/!|！/g, generateEmoji())
 
   if (tag === 'x')
     return word
@@ -28,12 +27,12 @@ function convertWord({ word, tag }: TaggedWord, level: number): string {
     return word.replace(/./g, '〇')
 
   if (tag === 'v' && probably(level))
-    return word + fragments.emoji
+    return word + generateEmoji()
 
   if (probably(level))
-    return word[0] + fragments.ellipsis(level) + word
+    return word[0] + generateEllipsis(level) + word
 
-  return fragments.ellipsis(level) + word
+  return generateEllipsis(level) + word
 }
 
 export function chs2yin(sentence: string, level = 0.7) {
